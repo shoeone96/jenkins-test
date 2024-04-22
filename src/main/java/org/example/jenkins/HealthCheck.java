81fdd31157487b6ee5830f212848cdaadb3523fa
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-04-22 16:19:56 +0900

    init

---
+++ src/main/java/org/example/jenkins/HealthCheck.java
@@ -0,0 +1,21 @@
+package org.example.jenkins;
+
+import java.util.Objects;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+    
+    @GetMapping
+    public ResponseEntity<String> checkFine(@RequestParam(required = false) String word){
+        if(Objects.isNull(word)){
+            return ResponseEntity.ok("fine thank you and you?");
+        }
+
+        return ResponseEntity.ok("this is what you give me" + word);
+    }
+
+}
